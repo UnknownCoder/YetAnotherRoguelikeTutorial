33298278c471adbd3ffc329b793efbff0514d920
Pawnxing <matthew.a.cox@gmail.com>
2022-08-14 22:33:10 -0400

    Part 4 - Field of View

--- tile_types.py
+++ tile_types.py
@@ -17,6 +17,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool),    # True if this tile can be walked over
         ("transparent", np.bool), # True if this tile doesn't block FOV
         ("dark", graphic_dt),     # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),    # Graphics for when this tile is in FOV
     ]
 )
 
@@ -26,21 +27,32 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types"""
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
-RGB_COLOR = {
-    "WHITE_RGB": (255, 255, 255),
-    "COLOR1_RGB": (50, 50, 150),
-    "COLOR2_RGB": (0, 0, 100),
-}
 
+# SHROUD represents unexplored, unseen tiles
+WHITE_RGB  = (255, 255, 255)
+BLACK_RGB  = (0, 0, 0)
+GOLD_RGB   = (200, 180, 50)
+CLAY_RGB   = (130, 110, 50)
+PURPLE_RGB = (50, 50, 150)
+BLUE_RGB   = (0, 0, 100)
+
+SHROUD = np.array((ord(" "), WHITE_RGB, BLACK_RGB), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), RGB_COLOR["WHITE_RGB"], RGB_COLOR["COLOR1_RGB"])
+    walkable=True, 
+    transparent=True,
+    dark=(ord(" "), WHITE_RGB, PURPLE_RGB),
+    light=(ord(" "), WHITE_RGB, GOLD_RGB),
 )
 
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), RGB_COLOR["WHITE_RGB"], RGB_COLOR["COLOR2_RGB"])
+    walkable=False, 
+    transparent=False, 
+    dark=(ord(" "), WHITE_RGB, BLUE_RGB),
+    light=(ord(" "), GOLD_RGB, CLAY_RGB),
 )
